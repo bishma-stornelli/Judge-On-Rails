54d7f804cff192c2d7e0a561f7bfc9069f2c6fe8
tamerdark <tamerdark@gmail.com>
2012-06-20 20:59:09 -0400

    Added confirmation for password, email, encrypt password and show for flash on the main layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password_digest, :username
+  attr_accessible :email, :email_confirmation, :password, :password_confirmation, :password_digest, :username
+
+  
+  # Validations for username
+  validates :username, presence: true, uniqueness: true
+  
+  # Validations for email
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, confirmation: true, format: { with: VALID_EMAIL_REGEX }
+  
+  # Validations for password
+  has_secure_password
 end
